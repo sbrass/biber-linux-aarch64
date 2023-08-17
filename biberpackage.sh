9821508d210d72acc979a9be82238e881236d920
Alexander Krumeich <alexander@krumeich.de>
2023-08-17 14:18:48 +0200

    Add support for CTAN package
    Call "make ctan" to build the whole package from scratch.

---
+++ biberpackage.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+BIBER_VERSION=$(docker run --rm -v $(pwd):/usr/local/bin debian:stable-slim /usr/local/bin/biber --version | sed "s,biber version: \([[:digit:]]\+\.[[:digit:]]\+\).*,\1,")
+PRODUCT=biber
+METAPLATFORM=linux
+METAPLATFORM_DIR=${PRODUCT}-${METAPLATFORM}
+ARCHIVE=${PRODUCT}-${BIBER_VERSION}-${METAPLATFORM}_aarch64.tar.gz
+
+rm -rf ${METAPLATFORM_DIR}
+mkdir -p ${METAPLATFORM_DIR}
+tar cf ${METAPLATFORM_DIR}/${ARCHIVE} biber
+
+cat >${METAPLATFORM_DIR}/README <<EOF
+These are biber binaries for the ${METAPLATFORM} platform.
+See https://ctan.org/pkg/biber for documentation, sources, and all else.
+EOF
+
+tar czf ${METAPLATFORM_DIR}.tar.gz ${METAPLATFORM_DIR}
+
